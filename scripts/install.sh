ecd409e93b0591b2b1c8f17928c2e4ba3d9b43b2
Derek Schafer <schaferderek96@gmail.com>
2026-05-01 08:36:48 -0700

    Updated install script to account for more variants

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,15 +7,23 @@ usage() {
     echo "Usage: $0 [-T value] [-B value]"
     echo " -T Integer that specifies build system: 0 (Tioga, default), 1 (Tuoloumne), 2 (Frontier)"
     echo " -B Build mode: 0 (Debug, default), 1 (Release), 2 (RelWithDebInfo)"
+    echo " -R String for rocm module (default is system specific)"
+    echo " -L String for libfabric version number (Possible values: 1.22.0, 2.1, SYSTEM)"
 }
 
-while getopts ":T:B:" opt; do
+while getopts ":T:B:R:L:" opt; do
     case $opt in
         T)
             VERSION="$OPTARG"
             ;;
         B)
             BUILD_MODE="$OPTARG"
+            ;;
+        R)
+            ROCM_VERSION="$OPTARG"
+            ;;
+        L)
+            LIBFABRIC_VERSION="$OPTARG"
             ;;
         *)
             usage
@@ -30,32 +38,51 @@ if [ -z $VERSION ]; then
     VERSION=0
 fi
 
+# Prepare System Specific variables
 if [ "$VERSION" -eq 0 ]; then
     SYSTEM=tioga
     GPU_ARCH=gfx90a
-    LIBFABRIC=/opt/cray/libfabric/2.1/
-    module load rocm "craype-accel-amd-${GPU_ARCH}" libfabric/2.1
+    LIBFABRIC_DEFAULT=2.1
 elif [ "$VERSION" -eq 1 ]; then
     SYSTEM=tuolumne
     GPU_ARCH=gfx942
-    LIBFABRIC=/opt/cray/libfabric/2.1/
-    module load rocmi/6.4.3 "craype-accel-amd-${GPU_ARCH}" libfabric/2.1
+    LIBFABRIC_DEFAULT=2.1
 elif [ "$VERSION" -eq 2 ]; then
     SYSTEM=frontier
     GPU_ARCH=gfx90a
-    LIBFABRIC=/opt/cray/libfabric/1.22.0/
-    module load cce/20.0.0 rocm/6.4.2 "craype-accel-amd-${GPU_ARCH}"
-    echo $LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH=${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
-    CMAKE_EXTRAS=( "-DCMAKE_EXE_LINKER_FLAGS=${PE_MPICH_GTL_DIR_amd_gfx90a} ${PE_MPICH_GTL_LIBS_amd_gfx90a}" )
-    echo $LD_LIBRARY_PATH
+    LIBFABRIC_DEFAULT=1.22.0
+
 else
     echo "Invalid system specified, stopping."
     exit 1
 fi
 
 echo -e "Running ${CYAN}$SYSTEM${RESET} version"
+
+# P
+if [ -z $ROCM_VERSION ]; then
+    ROCM_VERSION="rocm"
+fi 
+
+if [ -z $LIBFABRIC_VERSION ]; then
+    LIBFABRIC_MODULE="libfabric/${LIBFABRIC_DEFAULT}"
+    LIBFABRIC_DIR="/opt/cray/libfabric/${LIBFABRIC_DEFAULT}"
+elif [ "$LIBFABRIC_VERSION" = "SYSTEM" ]; then
+    LIBFABRIC_DIR="/usr/lib64"
+else
+    LIBFABRIC_MODULE="libfabric/${LIBFABRIC_VERSION}"
+    LIBFABRIC_DIR="/opt/cray/libfabric/${LIBFABRIC_VERSION}"
+fi
+
+# Load Modules
+module load "craype-accel-amd-${GPU_ARCH}" "${ROCM_VERSION}" ${LIBFABRIC_MODULE}
 module list
+
+# Extra setup for Frontier
+if [ "$VERSION" -eq 2 ]; then
+    export LD_LIBRARY_PATH=${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
+    CMAKE_EXTRAS=( "-DCMAKE_EXE_LINKER_FLAGS=${PE_MPICH_GTL_DIR_amd_gfx90a} ${PE_MPICH_GTL_LIBS_amd_gfx90a}" )
+fi
 
 if [ -z $BUILD_MODE ]; then
     BUILD_MODE=0
@@ -77,7 +104,7 @@ if [ -d $DIR_TO_BUILD ]; then
 fi
 mkdir $DIR_TO_BUILD && cd $DIR_TO_BUILD
 
-cmake -DUSE_HIP_BACKEND=ON -DUSE_CXI_BACKEND=ON -DLIBFABRIC_PREFIX=${LIBFABRIC} \
+cmake -DUSE_HIP_BACKEND=ON -DUSE_CXI_BACKEND=ON -DLIBFABRIC_PREFIX=${LIBFABRIC_DIR} \
       -DCMAKE_HIP_ARCHITECTURES=${GPU_ARCH} -DCMAKE_INSTALL_PREFIX=${HOME}/apps/stream_trigger \
       "${CMAKE_EXTRAS[@]}" -DCMAKE_BUILD_TYPE=$MODE ..
 
